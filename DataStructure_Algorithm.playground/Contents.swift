5efb6d0bcab55740090e3723377a040172c40ade
백상휘 <sanghwiback@gmail.com>
2022-07-05 11:07:05 +0900

    Add push, append, node, insert to LinkedList

--- DataStructure_Algorithm.playground/Contents.swift
+++ DataStructure_Algorithm.playground/Contents.swift
@@ -1,10 +1,31 @@
 import Foundation
 
-let node1 = Node(value: 1)
-let node2 = Node(value: 2)
-let node3 = Node(value: 3)
+var pushList = LinkedList<Int>()
 
-node1.next = node2
-node2.next = node3
+pushList.push(3)
+pushList.push(2)
+pushList.push(1)
 
-print(node1)
+print(pushList)
+
+var appendList = LinkedList<Int>()
+
+appendList.append(1)
+appendList.append(2)
+appendList.append(3)
+
+print(appendList)
+
+var insertList = LinkedList<Int>()
+
+insertList.push(3)
+insertList.push(2)
+insertList.push(1)
+
+print("Before insert: \(insertList)")
+var middleNode = insertList.node(of: 1)! // 2?
+
+for _ in 1...4 {
+    middleNode = insertList.insert(-1, after: middleNode)
+}
+print("After insert: \(insertList)")
--- DataStructure_Algorithm.playground/Sources/LinkedList/LinkedList.swift
+++ DataStructure_Algorithm.playground/Sources/LinkedList/LinkedList.swift
@@ -11,6 +11,46 @@ public struct LinkedList<Value> {
     public var isEmpty: Bool {
         head == nil
     }
+    
+    public mutating func push(_ value: Value) {
+        head = Node(value: value, next: head)
+        if tail == nil {
+            tail = head
+        }
+    }
+    
+    public mutating func append(_ value: Value) {
+        guard isEmpty == false else {
+            push(value)
+            return
+        }
+        
+        tail?.next = Node(value: value)
+        tail = tail?.next
+    }
+    
+    public func node(of index: Int) -> Node<Value>? {
+        var currentNode = head
+        var currentIndex = 0
+        
+        while currentNode != nil && currentIndex < index {
+            currentNode = currentNode?.next
+            currentIndex += 1
+        }
+        
+        return currentNode
+    }
+    
+    @discardableResult
+    public mutating func insert(_ value: Value, after node: Node<Value>) -> Node<Value> {
+        guard tail !== node else {
+            append(value)
+            return tail!
+        }
+        
+        node.next = Node(value: value, next: node.next) // 파라미터 node의 다음 node를 새로 만들고 next node는 파라미터 node의 next node로 바꿔준다.
+        return node.next! // insert 된 Node.
+    }
 }
 
 extension LinkedList: CustomStringConvertible {
