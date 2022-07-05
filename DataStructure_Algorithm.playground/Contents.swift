426f0b2277056248b3857406632bac00863735a8
백상휘 <sanghwiback@gmail.com>
2022-07-05 09:54:44 +0900

    Add Node and LinkedList

--- DataStructure_Algorithm.playground/Contents.swift
+++ DataStructure_Algorithm.playground/Contents.swift
@@ -1,1 +1,10 @@
 import Foundation
+
+let node1 = Node(value: 1)
+let node2 = Node(value: 2)
+let node3 = Node(value: 3)
+
+node1.next = node2
+node2.next = node3
+
+print(node1)
---
+++ DataStructure_Algorithm.playground/Sources/LinkedList/LinkedList.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+// 이중 연결 리스트
+public struct LinkedList<Value> {
+    
+    public var head: Node<Value>?
+    public var tail: Node<Value>?
+    
+    public init() { }
+    
+    public var isEmpty: Bool {
+        head == nil
+    }
+}
+
+extension LinkedList: CustomStringConvertible {
+    
+    public var description: String {
+        guard let head = head else {
+            return "Empty List"
+        }
+        
+        return String(describing: head)
+    }
+}
