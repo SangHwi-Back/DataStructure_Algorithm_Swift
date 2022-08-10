b4caca20a4192d6a6e8b7e8d6aec22fcb38a910a
백상휘 <sanghwiback@gmail.com>
2022-08-10 09:45:23 +0900

    Add some access Controls
    - Playground 환경 상 오류가 발생함에 따른 조치입니다.

--- DataStructure_Algorithm.playground/Sources/Tree/TraversableBinaryMode.swift
+++ DataStructure_Algorithm.playground/Sources/Tree/TraversableBinaryMode.swift
@@ -11,19 +11,19 @@ public protocol TraversableBinaryMode {
 }
 
 extension TraversableBinaryMode {
-    func traverseInOrder(visit: (Element) -> Void) {
+    public func traverseInOrder(visit: (Element) -> Void) {
         leftChild?.traverseInOrder(visit: visit)
         visit(value)
         rightChild?.traverseInOrder(visit: visit)
     }
     
-    func traversePreOrder(visit: (Element) -> Void) {
+    public func traversePreOrder(visit: (Element) -> Void) {
         visit(value)
         leftChild?.traversePreOrder(visit: visit)
         rightChild?.traversePreOrder(visit: visit)
     }
     
-    func traversePostOrder(visit: (Element) -> Void) {
+    public func traversePostOrder(visit: (Element) -> Void) {
         leftChild?.traversePostOrder(visit: visit)
         rightChild?.traversePostOrder(visit: visit)
         visit(value)
