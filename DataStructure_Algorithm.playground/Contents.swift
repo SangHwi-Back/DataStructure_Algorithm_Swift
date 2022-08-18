41c353047f7473c8f89124a345b5291aaf41789c
백상휘 <sanghwiback@gmail.com>
2022-08-18 11:56:12 +0900

    Add Heap

--- DataStructure_Algorithm.playground/Contents.swift
+++ DataStructure_Algorithm.playground/Contents.swift
@@ -1,14 +1,7 @@
 import Foundation
 
-let array = [1,16,15,17,19,22,24,31,105,150]
+var heap = Heap(sort: >, elements: [1,12,3,4,1,6,8,7])
 
-let search31 = array.firstIndex(of: 31)
-let binarySearch31 = array.binarySearch(for: 31)
-
-print("firstIndex(of:): \(String(describing: search31))")
-print("binarySearch(for:): \(String(describing: binarySearch31))")
-
-let array2 = [1,2,3,3,3,4,5,5]
-if let indices = findIndices(of: 3, in: array2) {
-    print(indices)
+while !heap.isEmpty {
+    print(heap.remove()!)
 }
