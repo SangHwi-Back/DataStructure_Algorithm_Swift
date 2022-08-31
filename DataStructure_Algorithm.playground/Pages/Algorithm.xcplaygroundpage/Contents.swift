90e99f421007d0d2a60ba9705172110ad7a9624a
백상휘 <sanghwiback@gmail.com>
2022-08-31 11:18:04 +0900

    MergeSort Challenge

--- DataStructure_Algorithm.playground/Pages/Algorithm.xcplaygroundpage/Contents.swift
+++ DataStructure_Algorithm.playground/Pages/Algorithm.xcplaygroundpage/Contents.swift
@@ -1,5 +1,24 @@
 import Foundation
 
-var array = [7,2,6,3,9]
-print("Original: \(array)")
-print("Merge sorted: \(mergeSort(array))")
+let size = 1024
+var values: [Int] = []
+// 1
+for i in 0 ..< size {
+    values.append(i)
+}
+
+//var sizes = size
+//var result = [0, size-1]
+//result.reserveCapacity(1024)
+//while result.count <= size {
+//    result.insert((result.max() ?? 0)-result.count/2, at: result.count/2)
+//    result.insert((result.min() ?? 0)+result.count/2, at: result.count/2)
+//}
+
+var array1 = [1, 2, 3, 4, 5, 6, 7, 8]
+var array2 = [1, 3, 4, 5, 5, 6, 7, 7]
+
+for element in merge(array1, array2) {
+    print(element)
+}
+
--- DataStructure_Algorithm.playground/Pages/Algorithm.xcplaygroundpage/Sources/Algorithm/SortingAlgorithms/MergeSort/MergeSort.swift
+++ DataStructure_Algorithm.playground/Pages/Algorithm.xcplaygroundpage/Sources/Algorithm/SortingAlgorithms/MergeSort/MergeSort.swift
@@ -46,3 +46,40 @@ private func merge<Element>(_ left: [Element], _ right: [Element]) -> [Element] 
     
     return result
 }
+
+public func merge<T: Sequence>(_ first: T, _ second: T) -> AnySequence<T.Element> where T.Element: Comparable {
+    var result: [T.Element] = []
+    
+    var firstIterator = first.makeIterator()
+    var secondIterator = second.makeIterator()
+    
+    var firstNextValue = firstIterator.next()
+    var secondNextValue = secondIterator.next()
+    
+    while let first = firstNextValue, let second = secondNextValue {
+        if first < second {
+            result.append(first)
+            firstNextValue = firstIterator.next()
+        } else if first > second {
+            result.append(second)
+            secondNextValue = secondIterator.next()
+        } else {
+            result.append(first)
+            result.append(second)
+            firstNextValue = firstIterator.next()
+            secondNextValue = secondIterator.next()
+        }
+    }
+    
+    while let first = firstNextValue {
+        result.append(first)
+        firstNextValue = firstIterator.next()
+    }
+    
+    while let second = secondNextValue {
+        result.append(second)
+        secondNextValue = secondIterator.next()
+    }
+    
+    return AnySequence<T.Element>(result)
+}
