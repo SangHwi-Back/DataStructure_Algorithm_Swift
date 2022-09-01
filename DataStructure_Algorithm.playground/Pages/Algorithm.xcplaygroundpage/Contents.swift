ac4614e18d2ffdd6d9709adfbf4a2ff7b078145a
백상휘 <sanghwiback@gmail.com>
2022-09-01 11:41:11 +0900

    Add RadixSort

--- DataStructure_Algorithm.playground/Pages/Algorithm.xcplaygroundpage/Contents.swift
+++ DataStructure_Algorithm.playground/Pages/Algorithm.xcplaygroundpage/Contents.swift
@@ -1,24 +1,6 @@
 import Foundation
 
-let size = 1024
-var values: [Int] = []
-// 1
-for i in 0 ..< size {
-    values.append(i)
-}
-
-//var sizes = size
-//var result = [0, size-1]
-//result.reserveCapacity(1024)
-//while result.count <= size {
-//    result.insert((result.max() ?? 0)-result.count/2, at: result.count/2)
-//    result.insert((result.min() ?? 0)+result.count/2, at: result.count/2)
-//}
-
-var array1 = [1, 2, 3, 4, 5, 6, 7, 8]
-var array2 = [1, 3, 4, 5, 5, 6, 7, 7]
-
-for element in merge(array1, array2) {
-    print(element)
-}
-
+var array = [88, 410, 1772, 20]
+print("Original array: \(array)")
+array.radixSort()
+print("Radix sorted: \(array)")
---
+++ DataStructure_Algorithm.playground/Pages/Algorithm.xcplaygroundpage/Sources/Algorithm/SortingAlgorithms/RadixSort/RadixSort.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+extension Array where Element == Int {
+    public mutating func radixSort() {
+        let base = 10
+        var done = false
+        var digits = 1
+        
+        while !done {
+            
+            done = true
+            
+            var buckets: [[Int]] = .init(repeating: [], count: base)
+            
+            forEach { number in
+                
+                let remainingPart: Int = number / digits
+                let digit = remainingPart % base
+                
+                buckets[digit].append(number)
+                
+                if remainingPart > 0 {
+                    done = false
+                }
+            }
+            
+            digits *= base
+            self = buckets.flatMap({ $0 })
+        }
+    }
+}
