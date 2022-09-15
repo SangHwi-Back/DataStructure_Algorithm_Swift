241a45fad6c62eaab0d69c21ce26fc25168d3b5e
백상휘 <sanghwiback@gmail.com>
2022-09-15 11:56:52 +0900

    Graphs Challenge

--- DataStructure_Algorithm.playground/Pages/DataStructure.xcplaygroundpage/Sources/Graph/Graph.swift
+++ DataStructure_Algorithm.playground/Pages/DataStructure.xcplaygroundpage/Sources/Graph/Graph.swift
@@ -45,3 +45,43 @@ extension Graph {
         }
     }
 }
+
+extension Graph where Element: Hashable {
+    public func numberOfPaths(from source: Vertex<Element>, to destination: Vertex<Element>) -> Int {
+        var numberOfPaths = 0
+        var visited = Set<Vertex<Element>>()
+        
+        paths(from: source,
+              to: destination,
+              visited: &visited,
+              pathCount: &numberOfPaths)
+        return numberOfPaths
+    }
+    
+    func paths(from source: Vertex<Element>,
+               to destination: Vertex<Element>,
+               visited: inout Set<Vertex<Element>>,
+               pathCount: inout Int) {
+        visited.insert(source)
+        
+        if source == destination {
+            pathCount += 1
+        } else {
+            let neighbors = edges(from: source)
+            
+            for edge in neighbors {
+                if !visited.contains(edge.destination) {
+                    
+                    paths(from: edge.destination,
+                          to: destination,
+                          visited: &visited,
+                          pathCount: &pathCount)
+                }
+            }
+        }
+        
+        visited.remove(source)
+    }
+}
+
+
